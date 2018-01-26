56cbbc743e55554afec1eba0ff016c45b79d7565
Matthieu Dubois <m-dubois@users.noreply.github.com>
2018-01-26 17:41:42 +0100

    fixed the zero arg issue

--- src/main/java/org/matt/tools/ProxyPasswordConfig.java
+++ src/main/java/org/matt/tools/ProxyPasswordConfig.java
@@ -5,6 +5,11 @@ public class ProxyPasswordConfig {
     public static void main(String[] args) {
 
         //abcdexyz0123456789&é"'(-è_çà)=$/\!~#{[|`\^@]}
-        System.out.println(CharacterEncoder.convert(args[0]));
+
+        if (args != null && args.length > 0) {
+            System.out.println(CharacterEncoder.convert(args[0]));
+        } else {
+            System.out.println("Usage: java -jar proxy-pwd-config.jar myPasswordToEncodeWithSp3ci@lChars!");
+        }
     }
 }
